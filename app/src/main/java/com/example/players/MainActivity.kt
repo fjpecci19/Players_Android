f74e71e60c6d77c9b47e804f75b99bf23894e957
[Franco <[fapeccisan@gmail.com]>
2023-08-30 14:24:14 -0400

    Scroll Horizontal de ítems del primer recycler view agregado

--- app/src/main/java/com/example/players/MainActivity.kt
+++ app/src/main/java/com/example/players/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.players.layoutManager = LinearLayoutManager(this)
+        binding.players.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         val playerlist = mutableListOf<Player>()
         playerlist.add(Player("https://pbs.twimg.com/profile_images/1550857936708280320/EiQrUKG4_400x400.jpg", "Jean", "Brasil"))
         playerlist.add(Player("https://versuspy2.fra1.cdn.digitaloceanspaces.com/versus/wp-content/uploads/2023/02/20122531/eduardo-brock-2.jpeg", "Brock", "Brasil"))
